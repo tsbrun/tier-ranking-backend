d2decf445d8243713b9b3a7fdd0c81070c0823cd
TSephanya Brun <tsbrun@icloud.com>
2021-10-19 21:04:00 -0400

    Set up belongs_to relationship - Item references Category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_20_000944) do
+ActiveRecord::Schema.define(version: 2021_10_20_010140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 2021_10_20_000944) do
     t.string "img"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_items_on_category_id"
   end
 
+  add_foreign_key "items", "categories"
 end

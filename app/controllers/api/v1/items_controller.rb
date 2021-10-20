eac5d2a0ed318159ab6cb333193117b1eb62d742
TSephanya Brun <tsbrun@icloud.com>
2021-10-20 14:37:29 -0400

    Render all items as json

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,3 +1,11 @@
 class Api::V1::ItemsController < ApplicationController
-    
+    def index 
+        items = Items.all.include(:category)
+        render json: {
+            data: ActiveModelSerializers::SerializableResource.new(items, each_serializer: ItemSerializer),
+            message: ['Items fetched successfully'],
+            status: 200,
+            type: 'Success'
+        }
+    end
 end

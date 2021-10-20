cded22d88aefe44f9fec00c163edda8dc6af3067
TSephanya Brun <tsbrun@icloud.com>
2021-10-20 14:39:44 -0400

    Items -> Item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ItemsController < ApplicationController
     def index 
-        items = Items.all.include(:category)
+        items = Item.all.include(:category)
         render json: {
             data: ActiveModelSerializers::SerializableResource.new(items, each_serializer: ItemSerializer),
             message: ['Items fetched successfully'],

9a257fa525fa9d0a00198c43bc7a60e9fdb8261a
TSephanya Brun <tsbrun@icloud.com>
2021-10-20 14:51:02 -0400

    Set :category attr to false for items without categories

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,10 +1,12 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :name, :img, :category
+  attributes :id, :name, :img
+  attributes :category
 
   def category
-    {
-      id: object.category.id,
-      title: object.category.title
-    }
+    # {
+    #   id: object.category.id,
+    #   title: object.category.title
+    # }
+    object.category || false 
   end
 end

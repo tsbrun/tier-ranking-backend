1bb1862d11866c8df738a0205e1c7c6f9246a654
TSephanya Brun <tsbrun@icloud.com>
2021-10-19 21:01:19 -0400

    Reformat seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,20 +7,18 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # set up Categories
-categories = Category.new([
-    {title: "Books"},
-    {title: "Movies"},
-    {title: "Fruit"}
-]).each { |c| c.save }
+books = Category.create(title: "Books")
+movies = Category.create(title: "Movies")
+fruit = Category.create(title: "Fruit")
 
 # set up Items 
-books = Category.find_by(title: "Books").items.build([
+books.items.build([
     {name: "The Metamorphosis", img: "https://images-na.ssl-images-amazon.com/images/I/51OoETNYFCL.jpg"},
     {name: "Pride and Prejudice", img: "http://prodimage.images-bn.com/pimages/9781499369748_p0_v3_s1200x630.jpg"},
     {name: "Dune", img: "https://images-na.ssl-images-amazon.com/images/I/41rgl-8wDsL._SX277_BO1,204,203,200_.jpg"}
 ]).each { |b| b.save }
 
-movies = Category.find_by(title: "Movies").items.build([
+movies.items.build([
     {name: "The Hunger Games", img: "https://m.media-amazon.com/images/M/MV5BMjA4NDg3NzYxMF5BMl5BanBnXkFtZTcwNTgyNzkyNw@@._V1_FMjpg_UX1000_.jpg"},
     {name: "Avengers: Endgame", img: "https://lumiere-a.akamaihd.net/v1/images/p_avengersendgame_19751_e14a0104.jpeg?region=0%2C0%2C540%2C810"},
     {name: "Alita: Battle Angel", img: "https://m.media-amazon.com/images/M/MV5BMTQzYWYwYjctY2JhZS00NTYzLTllM2UtZWY5ZTk0NmYwYzIyXkEyXkFqcGdeQXVyMzgxODM4NjM@._V1_.jpg"}

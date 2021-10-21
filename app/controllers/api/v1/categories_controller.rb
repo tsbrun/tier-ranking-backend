bec149489bfa29daec68a4a0b2bf05a20a3550c5
TSephanya Brun <tsbrun@icloud.com>
2021-10-21 15:45:37 -0400

    Create new category with strong params and render as json

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -10,12 +10,17 @@ class Api::V1::CategoriesController < ApplicationController
     end
 
     def create 
-        category = Category.new
+        @category = Category.new(category_params)
+        if @category.save 
+            render json: @category, status: :accepted
+        else
+            render json: { errors: @category.errors.full_messages }, status: :unprocessible_entity
+        end
     end
 
     params 
 
-    def new_category
+    def category_params
         params.permit(:title)
     end
 end

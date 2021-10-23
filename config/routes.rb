c2c77243b179a36507830dc61a6217e9a6daa3fd
TSephanya Brun <tsbrun@icloud.com>
2021-10-23 02:06:45 -0400

    Draw update route for items

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :categories, only: [:index, :create]
-      resources :items, only: [:index, :create]
+      resources :items, only: [:index, :create, :update]
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
